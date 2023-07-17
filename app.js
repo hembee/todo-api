2e81f44c89d2761afd898af851f38c6c478f5bc4
hembee <hembee999@gmail.com>
2023-07-17 12:39:09 +0100

    final

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
-import {globalErrorHandler} from "./src/utils/errorHandler.js"
+import { globalErrorHandler } from "./src/utils/errorHandler.js";
 import { config } from "./src/config/index.js";
 
 // Importing the User Routes
-import {router as userRouter} from "./src/router/user.route.js"
-import {router as taskRouter} from "./src/router/task.route.js"
+import { router as userRouter } from "./src/router/user.route.js";
+import { router as taskRouter } from "./src/router/task.route.js";
 // import userRouter, { obj as userObj, callName} from "./router/user.route.js"
 
 // console.log(userObj)
 // callName()
 // Creating the express app
-const app = express()
+const app = express();
 
 // Exposing environment variables
 // dotenv.config()
 
 // Database connection
-mongoose.connect(config.mongodb_connection_url).then(()=> console.log("Database connection established")).catch(e=> console.log("Mongo connection error: ", e.message))
-
+mongoose
+  .connect(config.mongodb_connection_url)
+  .then(() => console.log("Database connection established"))
+  .catch((e) => console.log("Mongo connection error: ", e.message));
 
 // Port configuration
 const port = config.port || 3000;
 
 // Middlewares
-app.use(morgan('tiny'))
-app.use(express.json())
+app.use(morgan("tiny"));
+app.use(express.json());
 
-// Routes 
-app.use('/api/v1/user', userRouter)
-app.use('/api/v1/task', taskRouter)
+// Routes
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/task", taskRouter);
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 // Setting up the express server
-app.listen(port, ()=>{
-  console.log(`Server runnning on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server runnning on port: ${port}`);
+});
--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -23,3 +23,4 @@ export const loginUserValidator = Joi.object({
   password: Joi.string().required()
 }).strict()
 
+
